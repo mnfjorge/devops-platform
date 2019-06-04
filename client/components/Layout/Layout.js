2a176506c4f2a8a7beef27d384024d5ae9f5ac6c
Marcio Nogueira Ferreira Jorge <mnfjorge@gmail.com>
2019-06-04 16:34:09 -0300

    Adding project structure

--- client/components/Layout/Layout.js
+++ client/components/Layout/Layout.js
@@ -1,21 +1,34 @@
 import React from 'react'
-import { Link, Switch, Route } from 'react-router-dom'
+import { NavLink, Switch, Route } from 'react-router-dom'
 import Home from '../Home/Home'
-import About from '../About/About'
+import ProjectsCreate from '../Projects/Create/ProjectsCreate'
 
 import './Layout.css'
 
 export default function Layout() {
   return (
     <div className="App">
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </div>
-      <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">DevOps Platform</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" exact activeClassName="active" to="/">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="/projects/create">Create Project</NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="container-fluid">
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/about" exact component={About} />
+          <Route path="/projects/create" exact component={ProjectsCreate} />
         </Switch>
       </div>
     </div>
